5f48d36424405a38c946ccf479e6bd188e81c9db
Xavier Hayes <xavier.hayes@southeasttech.edu>
2024-02-18 07:52:04 -0600

    Created ToDoViewModel

---
+++ ToDoList/Models/ToDoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+    public class ToDoViewModel
+    {
+
+        public Filters Filters { get; set; } = null!;
+        public List<Status> Statuses { get; set; } = null!; 
+        public List<Category> Categories { get; set; } = null!;
+        public Dictionary<string, string> DueFilters { get; set; } = null!;
+        public List<ToDo> Tasks { get; set; } = null!; public ToDo CurrentTask { get; set; } = null!; // used for Add
+
+
+    }
+}
